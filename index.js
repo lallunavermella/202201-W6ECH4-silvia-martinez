8408d88512b8cd36c65ee438b8cdb22757085a6f
Silvia Martinez <sil@silvia.com>
2022-02-17 19:33:52 +0100

    Update server

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const debug = require("debug")("ch-things:root");
 const upServer = require("./server");
 
-const port =
-  process.env.SERVER_PORT ||
-  4000(async () => {
-    try {
-      await upServer(port);
-      debug("it work");
-    } catch (error) {
-      debug("Not func");
-    }
-  })();
+const port = process.env.SERVER_PORT || 4000;
+
+(async () => {
+  try {
+    await upServer(port);
+    debug("it work");
+  } catch (error) {
+    debug(`Error`);
+  }
+})();
--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const debug = require("debug")("ch-things:server");
 const express = require("express");
 const morgan = require("morgan");
-const app = express;
 
-const upServer = (port) => {
-  return new Promise((resolve, reject) => {
-    app.listen(port, () => {
+const app = express();
+
+const upServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
       debug(`Server listening on http://localhost:${port}`);
       resolve();
     });
+
+    server.on("error", (error) => {
+      const errorMessage = `Couldn't start the server.${
+        error.code === "EADDRINUSE" ? ` Port ${port} busy` : ""
+      }`;
+      reject(new Error(errorMessage));
+    });
   });
-};
+
+app.use(morgan("dev"));
 
 module.exports = upServer;

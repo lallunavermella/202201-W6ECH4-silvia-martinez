864c8a960141791a54a907ddd6929163cdbff55b
Silvia Martinez <sil@silvia.com>
2022-02-17 20:26:35 +0100

    Created routes with endpoints

---
+++ server/routes/things.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/things", async (req, res) => {
+  const things = await Things.find();
+  res.json({ things });
+});
+
+router.get("/things/:idThing", async (req, res) => {
+  const thing = await Thing.find();
+  res.json({ thing });
+});
+
+router.delete("/things/:idThing", async (req, res) => {
+  const thing = await Thing.find();
+  res.json({ thing });
+});
+
+router.post("/things", async (req, res) => {
+  const Gatete = req.body;
+  const modifiedThing = await Thing.create(newThing);
+  res.status(201);
+  res.json(createdThing);
+});
+
+router.put("/things", async (req, res) => {
+  const newGatete = req.body;
+  const modifiedThing = await Thing.create(newThing);
+  res.status(201);
+  res.json(createdThing);
+});
